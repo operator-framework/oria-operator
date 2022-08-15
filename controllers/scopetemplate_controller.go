5b830018aa88613b4ffa7055581913d6670235ec
Rashmi Gottipati <chowdary.grashmi@gmail.com>
2022-08-15 14:37:32 -0400

    Updated logic to handle create/delete functionality
    Signed-off-by: Rashmi Gottipati <chowdary.grashmi@gmail.com>

--- controllers/scopetemplate_controller.go
+++ controllers/scopetemplate_controller.go
@@ -23,6 +23,7 @@ import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	k8sapierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -69,74 +70,21 @@ func (r *ScopeTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 	log.Log.Info("Getting ScopeTemplate", "name", in.Name)
 
-	scopeinstances := operatorsv1.ScopeInstanceList{}
-
-	if err := r.Client.List(ctx, &scopeinstances, client.InNamespace(req.Namespace)); err != nil {
-		return ctrl.Result{}, fmt.Errorf("list scope instances: %v", err)
+	// create ClusterRoles based on the ScopeTemplate
+	if err := r.createClusterRoles(ctx, in); err != nil {
+		return ctrl.Result{}, fmt.Errorf("create ClusterRoles: %v", err)
 	}
 
-	for i := range scopeinstances.Items {
-		sInstance := scopeinstances.Items[i]
-		if sInstance.Spec.ScopeTemplateName == in.Name {
-			log.Log.Info("ScopeInstance found that references ScopeTemplate", "name", in.Name)
-			// (todo): only create the associated cluster roles if a ScopeInstance exists that references ScopeTemplate
-		}
-	}
-
-	// siKey := types.NamespacedName{
-	// 	Namespace: req.Namespace,
-	// 	Name:      "scopeinstance-sample",
-	// }
-
-	// get the scope instance
-	// sInstance := &operatorsv1.ScopeInstance{}
-	// if err := r.Client.Get(ctx, siKey, sInstance); err != nil {
-	// 	return ctrl.Result{}, err
-	// }
-
-	// log.Log.Info("Getting ScopeInstance", "name", sInstance.Name)
-
-	var clusterRole *rbacv1.ClusterRole
-	// Create all the clusterRoles
-	for _, cr := range in.Spec.ClusterRoles {
-		clusterRole = &rbacv1.ClusterRole{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: cr.GenerateName,
-			},
-			Rules: cr.Rules,
-		}
-
-		// make scope template controller the owner of cluster role object
-		if err := controllerutil.SetOwnerReference(in, clusterRole, r.Scheme); err != nil {
-			return ctrl.Result{}, fmt.Errorf("error setting owner reference: %w", err)
-		}
-
-		log.Log.Info("Creating ClusterRole", "name", clusterRole.Name)
-		if err := r.Client.Create(ctx, clusterRole); err != nil {
-			log.Log.Error(err, "Failed to create ClusterRole", "name", clusterRole.GenerateName)
-			if errors.IsAlreadyExists(err) {
-				log.Log.Info("ClusterRole already exists", "name", clusterRole.Name)
+	// get the scope template to make sure it doesn't exist before deleting all of its associated ClusterRoles.
+	sTemplate := &operatorsv1.ScopeTemplate{}
+	if err := r.Client.Get(ctx, req.NamespacedName, sTemplate); err != nil {
+		if !k8sapierrors.IsNotFound(err) {
+			// Delete ClusterRoles associated with ScopeTemplate if/when the ScopeTemplate gets deleted
+			if err := r.deleteClusterRoles(ctx, in); err != nil {
+				return ctrl.Result{}, fmt.Errorf("delete ClusterRoles: %v", err)
 			}
 		}
-	}
-
-	crKey := types.NamespacedName{
-		Namespace: req.Namespace,
-		Name:      clusterRole.Name,
-	}
-
-	// get cluster role associated with the scope template
-	cRole := &rbacv1.ClusterRole{}
-	if err := r.Client.Get(ctx, crKey, cRole); err != nil {
-		return ctrl.Result{}, fmt.Errorf("get %q: %v", cRole.Name, err)
-	}
-	log.Log.Info("Getting ClusterRole", "name", cRole.Name)
-
-	// (todo): I think setting OwnerReference will take care of deleting associated cluster roles when ScopeTemplate gets deleted, not sure if we need to add delete call explicitly.
-	// delete cluster role associated with the scope template
-	log.Log.Info("Deleting ClusterRole associated with the ScopeTemplate", "name", cRole.Name)
-	if err := r.Client.Delete(ctx, cRole); err != nil && !apierrors.IsNotFound(err) {
-		return ctrl.Result{}, fmt.Errorf("delete %q: %v", cRole.Name, err)
+		return ctrl.Result{}, err
 	}
 
 	log.Log.Info("No ScopeTemplate error")
@@ -146,14 +94,10 @@ func (r *ScopeTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ScopeTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	enqueueScopeTemplate := handler.EnqueueRequestsFromMapFunc(r.mapToScopeTemplate)
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&operatorsv1.ScopeTemplate{}).
-		// Watch for changes to cluster roles created by Scope Template and trigger a Reconcile for the owner ScopeTemplate
-		// Watches(&source.Kind{Type: &operatorsv1.ScopeTemplate{}}, &handler.EnqueueRequestForOwner{
-		// 	OwnerType:    &operatorsv1.ScopeTemplate{},
-		// 	IsController: true}).
-		Watches(&source.Kind{Type: &operatorsv1.ScopeInstance{}}, enqueueScopeTemplate).
+		// Set up a watch for ScopeInstance to handle requeuing of requests for ScopeTemplate
+		Watches(&source.Kind{Type: &operatorsv1.ScopeInstance{}}, handler.EnqueueRequestsFromMapFunc(r.mapToScopeTemplate)).
 		Complete(r)
 }
 
@@ -162,13 +106,8 @@ func (r *ScopeTemplateReconciler) mapToScopeTemplate(obj client.Object) (request
 		return
 	}
 
-	// (todo): Check if obj can be converted into a scope instance.
-	// sInstance, ok := obj.(*operatorsv1.ScopeInstance)
-	// if !ok {
-	// 	return nil
-	// }
-
 	ctx := context.TODO()
+	// (todo): Check if obj can be converted into a scope instance.
 	scopeInstance := &operatorsv1.ScopeInstance{}
 	if err := r.Client.Get(ctx, types.NamespacedName{Namespace: obj.GetNamespace(), Name: scopeInstance.Spec.ScopeTemplateName}, scopeInstance); err != nil {
 		return nil
@@ -187,4 +126,80 @@ func (r *ScopeTemplateReconciler) mapToScopeTemplate(obj client.Object) (request
 	requests = append(requests, request)
 
 	return requests
+}
+
+func (r *ScopeTemplateReconciler) createClusterRoles(ctx context.Context, sTemplate *operatorsv1.ScopeTemplate) error {
+	scopeinstances := operatorsv1.ScopeInstanceList{}
+
+	if err := r.Client.List(ctx, &scopeinstances, client.InNamespace(sTemplate.Namespace)); err != nil {
+		return fmt.Errorf("list scope instances: %v", err)
+	}
+
+	var clusterRole *rbacv1.ClusterRole
+	// Create all the ClusterRoles
+	for i := range scopeinstances.Items {
+		sInstance := scopeinstances.Items[i]
+		if sInstance.Spec.ScopeTemplateName == sTemplate.Name {
+			log.Log.Info("ScopeInstance found that references ScopeTemplate", "name", sTemplate.Name)
+			for _, cr := range sTemplate.Spec.ClusterRoles {
+				clusterRole = &rbacv1.ClusterRole{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: cr.GenerateName,
+					},
+					Rules: cr.Rules,
+				}
+
+				// make scope template controller the owner of cluster role object
+				if err := controllerutil.SetOwnerReference(sTemplate, clusterRole, r.Scheme); err != nil {
+					return fmt.Errorf("error setting owner reference: %w", err)
+				}
+
+				// (todo): If ClusterRole already exists, then don't create it, just update it.
+				log.Log.Info("Creating ClusterRole", "name", clusterRole.Name)
+				if err := r.Client.Create(ctx, clusterRole); err != nil {
+					log.Log.Error(err, "Failed to create ClusterRole", "name", clusterRole.GenerateName)
+					if errors.IsAlreadyExists(err) {
+						log.Log.Info("ClusterRole already exists", "name", clusterRole.Name)
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (r *ScopeTemplateReconciler) deleteClusterRoles(ctx context.Context, sTemplate *operatorsv1.ScopeTemplate) error {
+	scopeinstances := &operatorsv1.ScopeInstanceList{}
+	if err := r.Client.List(ctx, scopeinstances); err != nil {
+		return fmt.Errorf("list scope instances: %v", err)
+	}
+
+	for _, sInstance := range scopeinstances.Items {
+		if sInstance.Spec.ScopeTemplateName == sTemplate.Name {
+			log.Log.Info("ScopeInstance found that references ScopeTemplate", "name", sTemplate.Name)
+			continue
+		}
+
+		for _, cr := range sTemplate.Spec.ClusterRoles {
+			crKey := types.NamespacedName{
+				Namespace: sTemplate.Namespace,
+				Name:      cr.GenerateName,
+			}
+
+			// get ClusterRole associated with the ScopeTemplate.
+			cRole := &rbacv1.ClusterRole{}
+			if err := r.Client.Get(ctx, crKey, cRole); err != nil {
+				return fmt.Errorf("get ClusterRole %q: %v", cRole.Name, err)
+			}
+			log.Log.Info("Got ClusterRole", "name", cRole.Name)
+
+			// delete ClusterRole associated with the ScopeTemplate.
+			log.Log.Info("Deleting ClusterRole associated with the ScopeTemplate", "name", cRole.Name)
+			if err := r.Client.Delete(ctx, cRole); err != nil && !apierrors.IsNotFound(err) {
+				return fmt.Errorf("delete %q: %v", cRole.Name, err)
+			}
+		}
+	}
+
+	return nil
 }
