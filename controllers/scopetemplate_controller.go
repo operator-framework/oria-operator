ec0a719baa7b02ea255f4495ace4bf9c5c804646
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2022-08-17 22:22:44 -0700

    Implement ScopeTemplate Controller
    Problem: AS part of OLM's initiative, we are moving towards descoping operator's. The OLM must manage the operator's Role Based Access Controls that will provide tooling that empowers cluster admins and operator authors to control which namespaces an operator reconciles resource events in.
    
    Solution: These changes are part of oria-operator's ScopeTemplate Controller. The main purpose of the oria-operator is to create operator's Cluster Role as specified in the ScopeTemplate CRD. This will enable CRUD operations for Scope Instance Controller.
    
    During reconciliation, it follows below steps
    1. Look for ScopeTemplate references Scope Instance or not. if it is then create ClusterRoles as specified in Scope Template else don't create it.
    2. Update happened in ScopeTemplate will perform the reconciliation process and do the crate, update, and delete events accordingly.
    3. Delete all the ClusterRoles if ScopeTemplate CRD gets deleted.
    
    updated according to review comments
    
    suqsh

--- controllers/scopetemplate_controller.go
+++ controllers/scopetemplate_controller.go
@@ -18,23 +18,40 @@ package controllers
 
 import (
 	"context"
-
-	rbacv1 "k8s.io/api/rbac/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"fmt"
+	"reflect"
 
 	operatorsv1 "awgreene/scope-operator/api/v1"
+	"awgreene/scope-operator/util"
+
+	"github.com/sirupsen/logrus"
+	rbacv1 "k8s.io/api/rbac/v1"
+	k8sapierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // ScopeTemplateReconciler reconciles a ScopeTemplate object
 type ScopeTemplateReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+
+	logger *logrus.Logger
 }
+
+const (
+	// generateNames are used to track each binding we create for a single scopeTemplate
+	clusterRoleGenerateKey = "operators.coreos.io/generateName"
+)
 
 //+kubebuilder:rbac:groups=operators.io.operator-framework,resources=scopetemplates,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operators.io.operator-framework,resources=scopetemplates/status,verbs=get;update;patch
@@ -54,29 +71,47 @@ func (r *ScopeTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 	log.Log.Info("Reconciling ScopeTemplate")
 
-	// get 	the scope template
-	in := &operatorsv1.ScopeTemplate{}
-	if err := r.Client.Get(ctx, req.NamespacedName, in); err != nil {
+	// get the scope template
+	st := &operatorsv1.ScopeTemplate{}
+	if err := r.Client.Get(ctx, req.NamespacedName, st); err != nil {
 		return ctrl.Result{}, err
 	}
 
-	// Create all the clusterRoles
-	for _, cr := range in.Spec.ClusterRoles {
-		clusterRole := &rbacv1.ClusterRole{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: cr.GenerateName,
-			},
-			Rules: cr.Rules,
+	scopeinstances := operatorsv1.ScopeInstanceList{}
+
+	if err := r.Client.List(ctx, &scopeinstances, client.InNamespace(st.Namespace)); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	listOptions := []client.ListOption{
+		&client.MatchingLabels{
+			scopeTemplateUIDKey: string(st.GetUID()),
+		},
+	}
+
+	for _, sInstance := range scopeinstances.Items {
+		if sInstance.Spec.ScopeTemplateName != st.Name {
+			continue
+		}
+		// create ClusterRoles based on the ScopeTemplate
+		log.Log.Info("ScopeInstance found that references ScopeTemplate", "name", st.Name)
+		if err := r.ensureClusterRoles(ctx, st); err != nil {
+			return ctrl.Result{}, fmt.Errorf("Error in create ClusterRoles: %v", err)
 		}
 
-		log.Log.Info("Creating ClusterRole", "name", clusterRole.GenerateName)
-		if err := r.Client.Create(ctx, clusterRole); err != nil {
-			if !errors.IsAlreadyExists(err) {
-				return ctrl.Result{}, err
-			}
-
-			// TODO: Handle potential update.
+		// Add requirement to delete old hashes
+		requirement, err := labels.NewRequirement(scopeTemplateHashKey, selection.NotEquals, []string{util.HashObject(st.Spec)})
+		if err != nil {
+			return ctrl.Result{}, err
 		}
+		listOptions = append(listOptions, &client.ListOptions{
+			LabelSelector: labels.NewSelector().Add(*requirement),
+		})
+		break
+	}
+
+	if err := r.deleteClusterRoles(ctx, listOptions...); err != nil {
+		return ctrl.Result{}, err
 	}
 
 	log.Log.Info("No ScopeTemplate error")
@@ -88,5 +123,110 @@ func (r *ScopeTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 func (r *ScopeTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&operatorsv1.ScopeTemplate{}).
+		// Set up a watch for ScopeInstance to handle requeuing of requests for ScopeTemplate
+		Watches(&source.Kind{Type: &operatorsv1.ScopeInstance{}}, handler.EnqueueRequestsFromMapFunc(r.mapToScopeTemplate)).
 		Complete(r)
+}
+
+func (r *ScopeTemplateReconciler) mapToScopeTemplate(obj client.Object) (requests []reconcile.Request) {
+	if obj == nil || obj.GetName() == "" {
+		return
+	}
+
+	ctx := context.TODO()
+	//(todo): Check if obj can be converted into a scope instance.
+	scopeInstance := &operatorsv1.ScopeInstance{}
+	if err := r.Client.Get(ctx, types.NamespacedName{Name: obj.GetName()}, scopeInstance); err != nil {
+		return nil
+	}
+
+	// Exit early if scopeInstance doesn't reference a scopeTemplate
+	if scopeInstance.Spec.ScopeTemplateName == "" {
+		return nil
+	}
+
+	// enqueue requests for ScopeTemplate based on Name and Namespace
+	request := reconcile.Request{
+		NamespacedName: types.NamespacedName{Namespace: obj.GetNamespace(), Name: scopeInstance.Spec.ScopeTemplateName},
+	}
+
+	requests = append(requests, request)
+
+	return requests
+}
+
+func (r *ScopeTemplateReconciler) ensureClusterRoles(ctx context.Context, st *operatorsv1.ScopeTemplate) error {
+	for _, cr := range st.Spec.ClusterRoles {
+		clusterRole := &rbacv1.ClusterRole{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: cr.GenerateName,
+				OwnerReferences: []metav1.OwnerReference{{
+					APIVersion: st.APIVersion,
+					Kind:       st.Kind,
+					Name:       st.GetObjectMeta().GetName(),
+					UID:        st.GetObjectMeta().GetUID(),
+				}},
+				Labels: map[string]string{
+					scopeTemplateUIDKey:    string(st.GetUID()),
+					scopeTemplateHashKey:   util.HashObject(st.Spec),
+					clusterRoleGenerateKey: cr.GenerateName,
+				},
+			},
+			Rules: cr.Rules,
+		}
+
+		crbList := &rbacv1.ClusterRoleList{}
+		if err := r.Client.List(ctx, crbList, client.MatchingLabels{
+			scopeTemplateUIDKey:    string(st.GetUID()),
+			clusterRoleGenerateKey: cr.GenerateName,
+		}); err != nil {
+			return err
+		}
+
+		// TODO: here to compare the clusterRoles against the expected values.
+		if len(crbList.Items) > 1 {
+			return fmt.Errorf("more than one ClusterRole found %s", cr.GenerateName)
+		}
+
+		// GenerateName is immutable, so create the object if it has changed
+		if len(crbList.Items) == 0 {
+			if err := r.Client.Create(ctx, clusterRole); err != nil {
+				return err
+			}
+			continue
+		}
+
+		existingCRB := &crbList.Items[0]
+
+		if util.IsOwnedByLabel(existingCRB.DeepCopy(), st) &&
+			reflect.DeepEqual(existingCRB.Rules, clusterRole.Rules) &&
+			reflect.DeepEqual(existingCRB.Labels, clusterRole.Labels) {
+			r.logger.Debug("Existing ClusterRoleBinding does not need to be updated")
+			return nil
+		}
+		existingCRB.Labels = clusterRole.Labels
+		existingCRB.OwnerReferences = clusterRole.OwnerReferences
+		existingCRB.Rules = clusterRole.Rules
+
+		if err := r.Client.Update(ctx, existingCRB); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *ScopeTemplateReconciler) deleteClusterRoles(ctx context.Context, listOptions ...client.ListOption) error {
+	clusterRoles := &rbacv1.ClusterRoleList{}
+	if err := r.Client.List(ctx, clusterRoles, listOptions...); err != nil {
+		// TODO: Aggregate errors
+		return err
+	}
+
+	for _, crb := range clusterRoles.Items {
+		// TODO: Aggregate errors
+		if err := r.Client.Delete(ctx, &crb); err != nil && !k8sapierrors.IsNotFound(err) {
+			return err
+		}
+	}
+	return nil
 }
--- util/util.go
+++ util/util.go
@@ -1,8 +1,14 @@
 package util
 
 import (
+	"fmt"
+	"hash"
+	"hash/fnv"
+
+	"github.com/davecgh/go-spew/spew"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
 
 // Owner is used to build an OwnerReference, and we need type and object metadata
@@ -40,4 +46,25 @@ func GetOwnerByRef(object metav1.Object, owner Owner) (ok bool) {
 		}
 	}
 	return false
+}
+
+// HashObject calculates a hash from an object
+func HashObject(obj interface{}) string {
+	hasher := fnv.New32a()
+	deepHashObject(hasher, &obj)
+	return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32()))
+}
+
+// DeepHashObject writes specified object to hash using the spew library
+// which follows pointers and prints actual values of the nested objects
+// ensuring the hash does not change when a pointer changes.
+func deepHashObject(hasher hash.Hash, objectToWrite interface{}) {
+	hasher.Reset()
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
+	printer.Fprintf(hasher, "%#v", objectToWrite)
 }
