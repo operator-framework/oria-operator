6fcd5725636289d6b2b8138f0e9bcd3520fe40af
Laxmikant Bhaskar Pandhare <47066536+laxmikantbpandhare@users.noreply.github.com>
2022-09-21 08:15:51 -0700

    removed unused variables due to which getting lint errors (#58)
    * removed unused variables due to which getting lint errors

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the operators v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=operators.io.operator-framework
+// +kubebuilder:object:generate=true
+// +groupName=operators.io.operator-framework
 package v1alpha1
 
 import (
