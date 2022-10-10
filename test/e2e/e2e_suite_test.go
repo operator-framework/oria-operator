f8a06bc971ae0f930af282a4e9972f13979fef11
Bryce Palmer <bpalmer@redhat.com>
2022-10-10 17:03:43 -0400

    add test folder
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

---
+++ test/e2e/e2e_suite_test.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"testing"
+	"time"
+
+	operatorsv1 "awgreene/scope-operator/api/v1alpha1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	cfg        *rest.Config
+	c          client.Client
+	kubeClient kubernetes.Interface
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	SetDefaultEventuallyTimeout(1 * time.Minute)
+	SetDefaultEventuallyPollingInterval(1 * time.Second)
+	RunSpecs(t, "E2E Suite")
+}
+
+var _ = BeforeSuite(func() {
+	cfg = ctrl.GetConfigOrDie()
+
+	scheme := runtime.NewScheme()
+	err := rbacv1.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+	err = operatorsv1.AddToScheme(scheme)
+	Expect(err).To(BeNil())
+
+	c, err = client.New(cfg, client.Options{Scheme: scheme})
+	Expect(err).To(BeNil())
+
+	kubeClient, err = kubernetes.NewForConfig(cfg)
+	Expect(err).To(BeNil())
+})
